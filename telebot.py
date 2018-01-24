4c5392c07a85c6ed56379138bf04003544e65c4f
Mendel <mendelmaleh@gmail.com>
2018-01-24 04:00:27 +0200

    more fixes

--- telebot.py
+++ telebot.py
@@ -5,7 +5,7 @@ import datetime
 class BotHandler:
 
     def __init__(self, token):
-        self.token = 516160274: AAEgyfeASRRdb0_XFDxtAOTtxQsjJjkt1bo
+        self.token = "516160274:AAEgyfeASRRdb0_XFDxtAOTtxQsjJjkt1bo"
         self.api_url = "https://api.telegram.org/bot{}/".format(token)
 
     def get_updates(self, offset=None, timeout=30):
